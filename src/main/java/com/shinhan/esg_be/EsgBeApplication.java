a14ff58aa715704cbdd1a33125c340f22f99b64f
sttarrynight <suwo5612@naver.com>
2026-03-29 23:10:16 +0900

    chore: 프론트 프로젝트 초기화

---
+++ src/main/java/com/shinhan/esg_be/EsgBeApplication.java
@@ -0,0 +1,13 @@
+package com.shinhan.esg_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsgBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsgBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shinhan/esg_be/EsgBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shinhan.esg_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsgBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
