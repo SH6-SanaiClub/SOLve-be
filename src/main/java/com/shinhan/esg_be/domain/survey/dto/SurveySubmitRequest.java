277815513e92f775f42279c8088ee6d83e48ed1e
sttarrynight <suwo5612@naver.com>
2026-04-16 12:04:26 +0900

    refactor: 설문조사 무한루프 버그 수정

--- src/main/java/com/shinhan/esg_be/domain/survey/dto/SurveySubmitRequest.java
+++ src/main/java/com/shinhan/esg_be/domain/survey/dto/SurveySubmitRequest.java
@@ -1,12 +1,32 @@
 package com.shinhan.esg_be.domain.survey.dto;
 
 import com.shinhan.esg_be.domain.user.entity.enums.UserType;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import lombok.Getter;
 
 @Getter
 public class SurveySubmitRequest {
 
-    @NotNull
     private UserType userType;
+
+    @Min(0)
+    @Max(2)
+    private Integer environmentWeight;
+
+    @Min(0)
+    @Max(2)
+    private Integer socialWeight;
+
+    @Min(0)
+    @Max(2)
+    private Integer financeWeight;
+
+    public boolean hasAllWeights() {
+        return environmentWeight != null && socialWeight != null && financeWeight != null;
+    }
+
+    public boolean hasAnyWeights() {
+        return environmentWeight != null || socialWeight != null || financeWeight != null;
+    }
 }
